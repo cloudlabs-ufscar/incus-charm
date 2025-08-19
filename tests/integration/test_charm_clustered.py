1fb7da5dbd73c0f046f7aa68398034fbee1e414e
Vinicius Rodrigues <vmarquesrodrigues25@gmail.com>
2025-08-19 20:42:24 -0300

    chore: charmcraft 3.0 base (deprecated) -> platform; update ops lib v0 apt
    feat: tests noble charm update
    
    vault update
    
    Signed-off-by: Vinicius Rodrigues <vmarquesrodrigues25@gmail.com>

--- tests/integration/test_charm_clustered.py
+++ tests/integration/test_charm_clustered.py
@@ -680,7 +680,7 @@ async def test_create_instance(ops_test: OpsTest):
     unit = application.units[0]
     target_node = ops_test.model.units["incus/3"].machine.hostname
     action = await unit.run(
-        f"incus launch images:ubuntu/22.04 {TEST_INSTANCE_NAME} --vm --storage ceph --quiet --target {target_node}"
+        f"incus launch images:ubuntu/24.04 {TEST_INSTANCE_NAME} --vm --storage ceph --quiet --target {target_node}"
     )
     await action.fetch_output()
     assert action.status == "completed", f"Action not completed: {action.results}"
